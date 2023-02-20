2ed4885c6d6d6f8dd78e6c82b976c195ff40b1bf
y2jsheikh <y2jsheikh@hotmail.com>
2023-02-20 12:11:01 +0500

    Print View Fixed

--- application/views/statement_of_need/statement_of_need_print_view.php
+++ application/views/statement_of_need/statement_of_need_print_view.php
@@ -23,14 +23,13 @@
             #print-btn {
                 float: right;
             }
-            #whole_content {
+            p, h4, h5 {
                 font-family: 'Times New Roman';
                 text-align: justify;
-
             }
         }
         @media print {
-            #whole_content {
+            p, h4, h5 {
                 font-family: 'Times New Roman' !important;
                 text-align: justify !important;
 
@@ -81,13 +80,14 @@
             <!--<div class="paf-banks-next">-->
             <div class="container">
                 <!--<div style="clear: both;"></div>-->
-                <div style="padding-top: 100px; padding-left: 15px; padding-right: 15px;">
+                <div style="padding-top: 150px; padding-left: 30px; padding-right: 30px;">
                     <div class="row">
-                        <div class="col-md-6">
+                        <div style="float: left; width: 50%;">
                             <h5><strong>No. F11-1/2013-NHS R&C/NOC</strong></h5>
                         </div>
-                        <div class="col-md-6">
-                            <h5 class="pull-right"><strong>Serial Number: 20/<?php echo date('Y'); ?><br/>Islamabad, <?php echo date('F d, Y', time()); ?></strong></h5>
+                        <div style="float: right; width: 50%;">
+                            <!--<h5 class="pull-right"><strong>Serial Number: 20/<?php /*echo date('Y'); */?><br/>Islamabad, <?php /*echo date('F d, Y', time()); */?></strong></h5>-->
+                            <h5 class="pull-right"><strong class="pull-right">Serial Number: 20/<?php echo date('Y'); ?></strong><br/><strong>Islamabad, <?php echo date('F d, Y', time()); ?></strong></h5>
                         </div>
                     </div>
                 </div>
@@ -104,20 +104,20 @@
                             $need = "a need";
                         }
                         ?>
-                        <h4 class="text-center"><strong><u><?php echo $heading ?></u></strong></h4><br/><br/>
+                        <h4 class="text-center"><strong><u><?php echo strtoupper($heading) ?></u></strong></h4><br/><br/>
 
                         <div class="col-md-12">
-                            <p><b>USMLE/ECFMG ID Number: <?php echo $result->ecfmg_no ?></b></p>
-                            <p><b>Name of Applicant for Visa: <?php echo $result->fullname ?></b></p>
                             <p>
+                                <b>USMLE/ECFMG ID Number: <?php echo $result->ecfmg_no ?></b>
+                                <br/>
+                                <b>Name of Applicant for Visa: <?php echo $result->fullname ?></b>
+                                <br/>
                                 There currently exists in Pakistan <?php echo $need ?> for qualified medical practitioners in the
                                 speciality of <?php echo $result->speciality ?>. <?php echo ucfirst($result->fullname) ?> has filed a written
                                 assurance with the government of this country that <?php echo $result->gender == 'Female' ? 'she' : 'he'; ?> will return to this country
                                 upon completion of training in the United States and intends to enter the practice of
                                 medicine in the speciality for which training is being sought.
-                            </p>
-                            <br/>
-                            <p>
+                                <br/><br/>
                                 This letter is valid for above said medical education program.
                             </p>
                             <br/>
